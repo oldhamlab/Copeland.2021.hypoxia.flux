6aadb630b7a51733d5e14dc7047a7d6659600d0b
William Oldham <wm.oldham@gmail.com>
2021-03-16 17:06:10 -0400

    finish cells_per_dna analysis

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,21 @@
 # functions.R
 
+path_to_data <- function(nm) {
+  system.file(
+    stringr::str_c("extdata/", nm),
+    package = "Copeland.2021.hypoxia.flux"
+  )
+}
+
+
+path_to_reports <- function(nm) {
+  system.file(
+    stringr::str_c("analysis/", nm),
+    package = "Copeland.2021.hypoxia.flux"
+  )
+}
+
+
 read_multi_excel <- function(excel_file) {
   sheets <- readxl::excel_sheets(excel_file)
   purrr::map(sheets, ~readxl::read_excel(excel_file, sheet = .x)) %>%
@@ -27,14 +43,111 @@ clean_technical_replicates <- function(tbl) {
     dplyr::ungroup()
 }
 
-data_file <-
-    system.file(
-      "extdata/dna-per-cell-number.xlsx",
-      package = "Copeland.2021.hypoxia.flux"
+make_std_curves <- function(df) {
+  df %>%
+    dplyr::filter(!is.na(.data$conc)) %>%
+    dplyr::select(where(~all(!is.na(.)))) %>%
+    dplyr::group_by(dplyr::across(-c(.data$conc, .data$value))) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      title = stringr::str_c(!!!rlang::syms(dplyr::groups(.)), sep = "_")
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      model = furrr::future_map(
+        .data$data,
+        ~lm(value ~ conc, data = .x, na.action = modelr::na.warn)
+      ),
+      summary = furrr::future_map(.data$model, broom::glance),
+      plots = furrr::future_map2(.data$data, .data$title, make_std_plots)
+    ) %>%
+    dplyr::group_by(
+      dplyr::across(
+        -c(.data$data, .data$title, .data$model, .data$summary, .data$plots)
+      )
     )
+}
 
-read_multi_excel(data_file) %>%
-  purrr::map(clean_technical_replicates) %>%
-  bind_rows(.id = "id") %>%
-  separate(id, c("cell_type", "volume"), sep = "_", convert = TRUE) %>%
-  mutate(cells = 1000 * cells)
+
+make_std_plots <- function(df, title = NULL) {
+  ggplot2::ggplot(df) +
+    ggplot2::aes(x = .data$conc, y = .data$value) +
+    ggplot2::geom_smooth(
+      method = "lm",
+      formula = y ~ x,
+      color = "gray20",
+      se = FALSE
+    ) +
+    ggplot2::geom_point(
+      size = 3,
+      alpha = 0.3,
+      color = "blue"
+    ) +
+    ggplot2::stat_summary(
+      fun = "mean",
+      size = 4,
+      geom = "point",
+      alpha = 0.8,
+      color = "blue"
+    ) +
+    ggplot2::labs(
+      x = "Concentration",
+      y = "Value",
+      title = title
+    )
+}
+
+
+interp_data <- function(tbl, std) {
+  tbl %>%
+    dplyr::filter(is.na(.data$conc)) %>%
+    dplyr::select(where(~all(!is.na(.)))) %>%
+    dplyr::group_by(dplyr::across(dplyr::group_vars(std))) %>%
+    tidyr::nest() %>%
+    dplyr::left_join(dplyr::select(std, .data$model)) %>%
+    dplyr::mutate(conc = purrr::map2(.data$data, .data$model, wmo::interpolate)) %>%
+    tidyr::unnest(c(.data$data, .data$conc)) %>%
+    dplyr::select(-c(.data$model, .data$value))
+}
+
+
+interpolate <- function(new_df, model) {
+  x <- stats::model.frame(model)[[deparse(model$terms[[3]])]]
+  p <- polynom::polynomial(stats::coefficients(model))
+  new_y <- as.list(new_df[[deparse(model$terms[[2]])]])
+  new_x <- unlist(lapply(new_y, function(y) {
+    roots <- solve(p, y)
+    roots <- round(roots, digits = 8)
+    root <- roots[which(Im(roots) == 0 & Re(roots) >= 0 & Re(roots) <= 1.25 * max(x))]
+    ifelse(identical(root, numeric(0)), NA, Re(root))
+  }))
+  new_x
+}
+
+
+clean_dna_per_cell <- function(filename) {
+  filename %>%
+    read_multi_excel() %>%
+    purrr::map(clean_technical_replicates) %>%
+    bind_rows(.id = "id") %>%
+    separate(id, c("cell_type", "volume"), sep = "_", convert = TRUE) %>%
+    mutate(cells = 1000 * cells)
+}
+
+
+calculate_cells_per_dna <- function(tbl) {
+  tbl %>%
+    filter(!(cell_type == "lf" & volume == "100" & cells == 300000)) %>%
+    filter(!(cell_type == "pasmc" & volume == "200" & cells == 400000)) %>%
+    group_by(cell_type, volume) %>%
+    nest() %>%
+    mutate(
+      model = map(data, ~lm(conc ~ 0 + cells, data = .x, na.action = modelr::na.warn)),
+      glance = map(model, broom::tidy)
+    ) %>%
+    unnest(c(glance)) %>%
+    select(cell_type, volume, slope = estimate) %>%
+    mutate(slope = 1/slope)
+}
+
+
--- _targets.R
+++ _targets.R
@@ -1,16 +1,35 @@
 # _targets.R
 
 #setup
+devtools::load_all()
 library(targets)
+library(tarchetypes)
 source("R/functions.R")
 
+conflicted::conflict_prefer("filter", "dplyr")
+
+options(tidyverse.quiet = TRUE)
+
+ppp <- TRUE
+
+future::plan(future::multisession(workers = future::availableCores() - 1))
+
 # target-specific options
-tar_option_set(packages = c(
-  "dplyr"
-))
+tar_option_set(
+  packages = c("tidyverse")
+)
 
 # list of target objects
 list(
-  tar_target(data, data.frame(x = sample.int(100), y = sample.int(100))),
-  tar_target(summary, summ(data)) # Call your custom functions as needed.
+  tar_target(dna_per_cell_file, path_to_data("dna-per-cell-number.xlsx")),
+  tar_target(dna_per_cell_raw, clean_dna_per_cell(dna_per_cell_file)),
+  tar_target(dna_per_cell_std, make_std_curves(dna_per_cell_raw)),
+  tar_target(dna_per_cell_clean, interp_data(dna_per_cell_raw, dna_per_cell_std)),
+  tar_target(cells_per_dna, calculate_cells_per_dna(dna_per_cell_clean)),
+  tar_render(
+    dna_per_cell_report,
+    path = path_to_reports("dna-per-cell.Rmd"),
+    params = list(print_plots = ppp),
+    output_dir = system.file("analysis/pdfs", package = "Copeland.2021.hypoxia.flux")
+  )
 )
